334f4889b178368e916752cea45821b94b509e2a
Aliou Bop <bopaliou1994@gmail.com>
2021-08-16 21:55:39 +0200

    quelques modifications sur l'entité document

---
+++ .history/src/Entity/Document_20210816214451.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DocumentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DocumentRepository::class)
+ */
+class Document
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateEnregistrement;
+
+    
+
+   
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $etat;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="documents")
+     */
+    private $user_document;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Image::class, mappedBy="document")
+     */
+    private $images;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $numero;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateExpiration;
+
+    
+
+    
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->dateEnregistrement;
+    }
+
+    public function setDateEnregistrement(?\DateTimeInterface $dateEnregistrement): self
+    {
+        $this->dateEnregistrement = $dateEnregistrement;
+
+        return $this;
+    }
+
+    
+
+   
+
+    public function getEtat(): ?bool
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(bool $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getUserDocument(): ?User
+    {
+        return $this->user_document;
+    }
+
+    public function setUserDocument(?User $user_document): self
+    {
+        $this->user_document = $user_document;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setDocument($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getDocument() === $this) {
+                $image->setDocument(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getNumero(): ?int
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(?int $numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getDateExpiration(): ?\DateTimeInterface
+    {
+        return $this->dateExpiration;
+    }
+
+    public function setDateExpiration(?\DateTimeInterface $dateExpiration): self
+    {
+        $this->dateExpiration = $dateExpiration;
+
+        return $this;
+    }
+
+    
+
+    
+}
--- migrations/Version20210816195000.php
+++ migrations/Version20210816195000.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210816185042 extends AbstractMigration
+final class Version20210816195000 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,12 @@ final class Version20210816185042 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE document ALTER date_expiration TYPE DATE');
-        $this->addSql('ALTER TABLE document ALTER date_expiration DROP DEFAULT');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE document ALTER date_expiration TYPE VARCHAR(255)');
-        $this->addSql('ALTER TABLE document ALTER date_expiration DROP DEFAULT');
     }
 }
