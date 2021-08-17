0dbdd9f72ee0fd4394ba8275f4a0fa1c4f76b466
Aliou Bop <bopaliou1994@gmail.com>
2021-08-17 13:01:14 +0200

    une petite modification sur l'entité User

--- .history/src/Entity/User_20210817130001.php
+++ .history/src/Entity/User_20210817130001.php
@@ -270,8 +270,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-    public function __toString()
-    {
-        return $this->prenom." ".$this->nom;
-    }
+    p
 }
--- .history/src/Entity/User_20210817130036.php
+++ .history/src/Entity/User_20210817130036.php
@@ -272,6 +272,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
     public function __toString()
     {
-        return $this->prenom." ".$this->nom;
+        return $this->prenom." ".$this->
     }
 }
